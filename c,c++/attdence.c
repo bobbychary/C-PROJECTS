be25943b41a21a016ac81c5318e4fbb5a278aa3c
Vikas chary <91590825+bobbychary@users.noreply.github.com>
2022-03-25 23:01:10 +0530

    Delete attdence.c

--- c,c++/attdence.c
+++
@@ -1,3 +0,0 @@
-https://www.codesdope.com/discussion/write-a-program-for-the-following-question/
-(it is for attdence):-
-https://www.tutorialspoint.com/program-to-find-minimum-number-of-lectures-to-attend-to-maintain-75-in-cplusplus
