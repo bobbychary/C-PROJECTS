e94b12af76f98a9b9cd74b536491dd1ff28b1108
Vikas chary <91590825+bobbychary@users.noreply.github.com>
2022-03-25 22:50:32 +0530

    Add files via upload

---
+++ c,c++/attdence.c
@@ -0,0 +1,3 @@
+https://www.codesdope.com/discussion/write-a-program-for-the-following-question/
+(it is for attdence):-
+https://www.tutorialspoint.com/program-to-find-minimum-number-of-lectures-to-attend-to-maintain-75-in-cplusplus
